9e773a8388914e5567646b7090e1742f7adb9379
luv4code-lfc <luv4code.lc@gmail.com>
2021-12-01 15:27:36 +0530

    Adding Beer Repositories. closes #6

---
+++ src/main/java/com/luv4code/lfcbeerservice/repositories/BeerRepository.java
@@ -0,0 +1,9 @@
+package com.luv4code.lfcbeerservice.repositories;
+
+import com.luv4code.lfcbeerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}
