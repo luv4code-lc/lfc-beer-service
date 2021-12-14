dc1dc0ec32b5d0c2563f6b2a365a817c0bae2457
luv4code-lfc <luv4code.lc@gmail.com>
2021-12-14 12:42:48 +0530

    implementing brewing service to trigger brewing

--- src/main/java/com/luv4code/lfcbeerservice/repositories/BeerRepository.java
+++ src/main/java/com/luv4code/lfcbeerservice/repositories/BeerRepository.java
@@ -5,11 +5,12 @@ import com.luv4code.lfcbeerservice.web.model.BeerStyleEnum;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.UUID;
 
-public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+public interface BeerRepository extends JpaRepository<Beer, UUID> {
     Page<Beer> findAllByBeerNameAndBeerStyle(String beerName, BeerStyleEnum beerStyle, Pageable pageable);
 
     Page<Beer> findAllByBeerName(String beerName, Pageable pageable);
---
+++ src/main/java/com/luv4code/lfcbeerservice/services/BrewingService.java
@@ -0,0 +1,42 @@
+package com.luv4code.lfcbeerservice.services;
+
+import com.luv4code.lfcbeerservice.config.JmsConfig;
+import com.luv4code.lfcbeerservice.domain.Beer;
+import com.luv4code.lfcbeerservice.events.BrewBeerEvent;
+import com.luv4code.lfcbeerservice.repositories.BeerRepository;
+import com.luv4code.lfcbeerservice.services.inventory.BeerInventoryService;
+import com.luv4code.lfcbeerservice.web.mappers.BeerMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class BrewingService {
+    private final BeerRepository beerRepository;
+    private final BeerInventoryService beerInventoryService;
+    private final JmsTemplate jmsTemplate;
+    private final BeerMapper beerMapper;
+
+
+    @Scheduled(fixedRate = 5000)
+    public void checkForLowInventory() {
+        List<Beer> beers = beerRepository.findAll();
+
+        beers.forEach(beer -> {
+            Integer invQOH = beerInventoryService.getOnhandInventory(beer.getId());
+
+            log.debug("Min On Hand is: " + beer.getMinOnHand());
+            log.debug("Inventory is: " + invQOH);
+
+            if (beer.getMinOnHand() >= invQOH) {
+                jmsTemplate.convertAndSend(JmsConfig.BREWING_REQUEST_QUEUE, new BrewBeerEvent(beerMapper.beerToBeerDto(beer)));
+            }
+        });
+    }
+}
